5bc30ccf3a99972da931d92cc49f706102b6629d
Dimitar Atanasov <dimitar.k.atanasov97@gmail.com>
2018-08-25 10:36:51 +0300

    Fixed Layout menu links

---
+++ RestaurantSystem.Web/wwwroot/js/AllEmployees.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $("#fire").click(function () {
+        if (confirm('Are You Sure to Delete this Employee Record ?')) {
+            $.ajax({
+                type: "GET",
+                url: '@Url.Action("Fire","Employees")/' + id,
+                success: function (data) {
+                    if (data.success) {
+                        window.ajax.reload();
+
+                        $.notify(data.message, {
+                            globalPosition: "top center",
+                            className: "success"
+                        })
+
+                    }
+
+                });
+        }
+    })
+})
