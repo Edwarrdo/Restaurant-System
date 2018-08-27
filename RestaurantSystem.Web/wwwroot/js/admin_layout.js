27d99dcba480a2186f96ea4637d725ad5ffd375a
Elian Kurtev <lqnko_55@outlook.com>
2018-08-27 12:36:00 +0300

    fixed img function sidebar

--- RestaurantSystem.Web/wwwroot/js/admin_layout.js
+++ RestaurantSystem.Web/wwwroot/js/admin_layout.js
@@ -5,42 +5,24 @@
             $('.footerParagraph').toggleClass('activeFooter');
     });
 
-    $(".hoverLink").hover(function () {
-        //$(".hoverLink").css({ "color:": " #69696A", "background": "#fff" })
-        $(this).attr("src", function (index, attr) {
-   
+
+    $('.hoverLink').mouseover(function (e) {
+        
+        $(this).find("img").attr("src", function (index, attr) {
             return attr.replace(".png", "_hovered.png");
         });
-        
-    }, function () {
-        $(this).attr("src", function (index, attr) {
+       
+    });;
+  
+
+    $('.hoverLink').mouseout(function () {
+       $(this).find("img").attr("src", function (index, attr) {
             return attr.replace("_hovered.png", ".png");
         });
-        });
-    
-    //$(".hoverLink0").hover(function () {
-    //    var index = $(".hoverLink img").attr("src")
-    //    $("img").attr("src", function (index, attr) {
+       
+    });
 
-    //        return attr.replace(".png", "_hovered.png");
-    //    });
-    //}, function () {
-    //    $("img").attr("src", function (index, attr) {
-    //        return attr.replace("_hovered.png", ".png");
-    //    });
-    //    });
 
-    //$(".hoverLink1").hover(function () {
-    //    var index = $(".hoverLink img").attr("src")
-    //    $("img").attr("src", function (index, attr) {
-
-    //        return attr.replace(".png", "_hovered.png");
-    //    });
-    //}, function () {
-    //    $("img").attr("src", function (index, attr) {
-    //        return attr.replace("_hovered.png", ".png");
-    //    });
-    //});
         $("select").attr('data-selected-text-format', 'count>3');
         $("select").attr('data-size', 5);
         $("select").attr('data-live-search', true);
