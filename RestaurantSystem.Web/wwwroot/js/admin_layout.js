e23168f848533d88dc276977c302972ddcc6da2d
Elian Kurtev <lqnko_55@outlook.com>
2018-08-30 10:26:04 +0300

    new

--- RestaurantSystem.Web/wwwroot/js/admin_layout.js
+++ RestaurantSystem.Web/wwwroot/js/admin_layout.js
@@ -1,29 +1,6 @@
 ﻿$(document).ready(function () {
 
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $('.footerParagraph').toggleClass('activeFooter');
-    });
-
-
-    $('.hoverLink').mouseover(function (e) {
-        
-        $(this).find("img").attr("src", function (index, attr) {
-            return attr.replace(".png", "_hovered.png");
-        });
-       
-    });;
-  
-
-    $('.hoverLink').mouseout(function () {
-       $(this).find("img").attr("src", function (index, attr) {
-            return attr.replace("_hovered.png", ".png");
-        });
-       
-    });
-
-
-        $("select").attr('data-selected-text-format', 'count>3');
+    $("select").attr('data-selected-text-format', 'count>3');
         $("select").attr('data-size', 5);
         $("select").attr('data-live-search', true);
 
